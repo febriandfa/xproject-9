41e489f70aad32f683f1a65060193f43b8173821
Febrian Daffa Eka Putra <febriandaffa123@gmail.com>
2023-07-16 11:05:52 +0700

    Some Update

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -4,11 +4,13 @@ import AboutAbout from "../components/about/AboutAbout";
 import Theme from "../components/about/Theme";
 import Background from "../components/Background";
 import Footer from "../components/Footer";
+import HeroTitle from "../components/HeroTitle";
 
 const About = () => {
   return (
     <div className="bg-gradient-to-b from-blue-400 to-blue-1000">
       <Navbar />
+      <HeroTitle title="X-Project 9.0" />
       <AboutAbout />
       <Theme />
       <Background />
--- src/pages/TechnoSymphony/TechSymphony.jsx
+++ src/pages/TechnoSymphony/TechSymphony.jsx
@@ -1,7 +1,18 @@
 import React from "react";
+import Navbar from "../../components/Navbar";
+import HeroTitle from "../../components/HeroTitle";
+import AboutAbout from "../../components/about/AboutAbout";
 
 const TechSymphony = () => {
-  return <div></div>;
+  return (
+    <div>
+      <Navbar />
+      <HeroTitle title="Techno Symphony" />
+      <div className="bg-gradient-to-b from-blue-1000 to-blue-400 pt-36">
+        <AboutAbout />
+      </div>
+    </div>
+  );
 };
 
 export default TechSymphony;
